263265798b4991a9e5ab16436939a58ac0659734
hsparrow <hsparrowww@yahoo.com>
2017-11-10 14:05:46 +1030

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.opencv.core.Core;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/MainFrame.fxml"));
+            BorderPane root = loader.load();
+            Scene scene = new Scene(root, 1200, 800);
+
+            primaryStage.setTitle("Face Recognition");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            Controller controller = loader.getController();
+
+            primaryStage.setOnCloseRequest((event -> controller.setClosed()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        launch(args);
+    }
+}
